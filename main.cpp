f14d01abb968224320c2363a78108d566ea2d3d5
Paul <pauldromeasaurio@hotmail.com>
2018-07-31 14:16:00 -0500

    Centered screen function

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QScreen>
+#include <QRect>
+#include <QGuiApplication>
 
-void configureScreenPosition();
+void configureScreenPosition(MainWindow &w);
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
 
-    configureScreenPosition();
+
+    configureScreenPosition(w);
+
+    w.show();
 
     return a.exec();
 }
 
-void configureScreenPosition(){
-
+void configureScreenPosition(MainWindow &w){
+    //Set window in the center
+    QRect screenGeometry = QGuiApplication::screens().at(0)->geometry();
+    int x = (screenGeometry.width()- w.width()) / 2;
+    int y = (screenGeometry.height()- w.height()) / 2;
+    w.move(x, y);
 }
